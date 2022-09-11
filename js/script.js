3e5ac5602d168bc9a2dbadbabd8aae84f33b48d0
ibroxim0607 <abdufattoxovibroxim@gmail.com>
2022-09-11 22:20:44 +0500

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,71 +1,105 @@
-let elSearchForm = document.querySelector(".js-search-form");
-let elSearchInput = elSearchForm.querySelector(".js-search-input");
-let elCountryInfo = document.querySelector(".js-country-info");
-let elSpinner = document.querySelector(".js-spinner");
+let elTemplate = document.getElementById("template").content;
+let elForm = document.querySelector(".js-form-search");
+let elInput = document.querySelector(".js-form-input");
+let elCountriesList = document.querySelector("js-countries-list");
+let elList = document.querySelector(".js-countries-list");
+let elModal = document.querySelector(".js-modal-wrapper");
+let elModalBody = elModal.querySelector(".modal-body");
 
+let countrties = [];
 
-// const renderCountry = datum => {
-//   elCountryInfo.innerHTML = "";
+// Template
+function renderCountries(country) {
+  elList.innerHTML = "";
 
-//   datum.forEach(data => {
-//     let elContryMiniInfo = document.createElement("div");
-//     elContryMiniInfo.innerHTML = `
-//       <p>Country: ${data.name.official}</p>
-//       <img src=${data.flags.png} alt=${data.name.official} width="300" height="200">`
+  let elTemplateFragment = document.createDocumentFragment();
+  country.forEach((country) => {
+    let newTemplate = elTemplate.cloneNode(true);
+    let templateItemClone = newTemplate.querySelector(".card");
 
-//     elCountryInfo.appendChild(elContryMiniInfo);
-//   })
-// }
+    templateItemClone.querySelector(".card-img-top").src = country.flags.png;
+    templateItemClone.querySelector(".card-img-top").alt = country.name.common;
 
+    templateItemClone.querySelector(".card-title").textContent =
+      country.name.common;
 
-const renderCountry = datum => {
-  elCountryInfo.innerHTML = ""
+    templateItemClone.querySelector(
+      ".card-text-1"
+    ).innerHTML = `<srtong class="fw-bold">Population:</srtong> ${country.population.toLocaleString(
+      "de-DE"
+    )}`;
 
-  datum.forEach(data => {
-    let elCountryMiniInfo = document.createElement("div")
-    let elCountryMiniImg = document.createElement("img")
-    let elCountryMiniTitle = document.createElement("h5")
+    templateItemClone.querySelector(
+      ".card-text-2"
+    ).innerHTML = `<strong class = "fw-bold">Sub-Region:</strong> ${country.subregion}`;
+    templateItemClone.querySelector(
+      ".card-text-3"
+    ).innerHTML = `<strong class = "fw-bold">Capital:</strong> ${country.capital}`;
+    templateItemClone.querySelector(
+      ".card-text-4"
+    ).innerHTML = `<strong class = "fw-bold">Map:</strong> <a href ="${country.maps.googleMaps}">Here</a> `;
 
-    elCountryMiniImg.src = data.flags.svg
-    elCountryMiniImg.classList.add("card-img-top")
+    templateItemClone.querySelector(".btn-primary").value = country.cca2;
+    // templateItemClone
+    //   .querySelector(".btn-primary")
+    //   .addEventListener("click", () => {
+    //     elModalBody.textContent = country.capital;
+    //   });
 
-    elCountryMiniTitle.textContent = data.name.official
-    elCountryMiniTitle.classList.add("card-title")
+    elTemplateFragment.append(templateItemClone);
+  });
 
-    elCountryMiniInfo.appendChild(elCountryMiniTitle, elCountryMiniImg)
-    elCountryInfo.appendChild(elCountryMiniInfo)
-  })
+  elList.append(elTemplateFragment);
 }
 
-const renderError = err => {
-  let elContryMiniInfo = document.createElement("div");
-    elContryMiniInfo.innerHTML = `
-      <p>${err}</p>
-    `
+// Error Function
+function errorFunction(err) {
+  elList.innerHTML = null;
 
-    elCountryInfo.appendChild(elContryMiniInfo);
+  let errItem = document.createElement("li");
+  errItem.className = "alert alert-danger";
+  errItem.textContent = err;
+
+  elList.append(errItem);
 }
 
-const getCountryData = country => {
+// FETCH
+function searchCountry(country) {
   fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(res => {
-      if (!res.ok) {
-      // if (res.status != 200) {
-        throw new Error(renderError("Xatolik yuz berdi."));
+    .then((res) => {
+      if (res.status != 200) {
+        throw new Error(error("Sorry your contry is not found"));
       }
 
-      return res.json()})
-    .then(data => renderCountry(data))
-    .catch(err => renderError(err))
+      return res.json();
+    })
+    .then((data) => {
+      countrties = data;
+      renderCountries(data);
+    })
+    .finally();
 }
 
-elSearchForm.addEventListener("submit", (evt) => {
+elForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
 
-  let value = elSearchInput.value.trim().toLowerCase();
+  elList.innerHTML = null;
 
+  let elInputValue = elInput.value.trim();
+  searchCountry(elInputValue);
 
-  getCountryData(value);
-})
+  elInput.value = "";
+});
 
-console.log();
+elList.addEventListener("click", (e) => {
+  if (e.target.matches(".js-query")) {
+    const countryCode = e.target.value;
+    const currentCountry = countrties.find(
+      (counrty) => counrty.cca2 == countryCode
+    );
+    const newP = document.createElement("p");
+    newP.textContent = currentCountry.capital;
+    newP.classList.add("text-capital");
+    elModalBody.append(newP);
+  }
+});
